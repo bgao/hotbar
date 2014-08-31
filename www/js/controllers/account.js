4238d185510fc8b5e7bc92992b1e2f96f5de1890
Kong Vang <vangko@gmail.com>
2014-08-31 18:48:28 -0400

    modified camera action code for account.js

--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -31,7 +31,7 @@ angular.module('hotbar.controllers')
       });
     };
 
-    $scope.changeProflePicture = function(){
+    $scope.changeProfilePicture = function(){
      navigator.camera.getPicture(cameraSuccess, cameraError, {
       quality : 50,
       destinationType : Camera.DestinationType.DATA_URL,
@@ -44,20 +44,197 @@ angular.module('hotbar.controllers')
       saveToPhotoAlbum: false
      });
 
-     function onSucess(imageURI){
-      user.picture = imageURI;
-     }
+     /////////////////////////////// camera functions copied from home.js
+      var cameraSuccess = function(imageFile) {
+      var _user = Global.get("user");
+      $timeout(function() {
+        var index = imageFile.lastIndexOf('/') + 1;
+        $scope.media = {
+          filename: _user.get('username') + "_" + imageFile.substr(index),
+          data: imageFile,
+          type: 'image/jpeg'
+          // src: "data:image/jpeg;base64,"+imageData
+        };
+        // debug
+        navigator.notification.alert(imageFile, null, "CameraSuccess");
+        navigator.notification.alert($scope.media.filename, null, "CameraSuccess");
+      });
+    };
+    var cameraError = function(error) {
+      if (error && error.code) {
+        $log.error("Capture error: " + error.code);
+        navigator.notification.alert('Error code: ' + error.code, null,
+                                     'Capture Error');
+      }
+      $scope.cleanup();
+    };
+    var captureSuccess = function(mediaFiles) {
+      var _user = Global.get("user");
+      // $scope.mediaSrc = $scope.trustSrc(mediaFiles[0].fullPath);
+      $scope.mediaSrc = mediaFiles[0].fullPath;
+      $timeout(function() {
+        navigator.notification.alert(mediaFiles, null, "CaptureSuccess");
+        navigator.notification.alert(mediaFiles.length, null, "CaptureSuccess");
+        $scope.media = {
+          filename: _user.get('username') + "_" +
+            mediaFiles[0].name,
+          data: mediaFiles[0].fullPath,
+          type: mediaFiles[0].type,
+          size: mediaFiles[0].size
+        };
+        // debug
+        navigator.notification.alert(mediaFiles[0].fullPath,
+                                     null,
+                                     "CameraSuccess:fullpath");
+        navigator.notification.alert($scope.media.filename,
+                                     null,
+                                     "CameraSuccess:filename");
+      });
+    };
+    var captureError = function(error) {
+      if (error && error.code) {
+        $log.error("Capture error: " + error.code);
+        navigator.notification.alert('Error code: ' + error.code, null,
+                                     'Capture Error');
+      }
+    };
 
-     function onFail(message){
-      navigator.notification.alert(message, null, "Error");
-     }
+    $scope.captureImage = function() {
+      var options = {
+        quality: 45,
+        // destinationType: Camera.DestinationType.DATA_URL
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA
+      };
+      navigator.camera.getPicture(cameraSuccess, cameraError, options);
+    };
 
+    $scope.getImage = function() {
+      var options = {
+        destinationType: Camera.DestinationType.DATA_URL,
+        // destinationType: Camera.DestinationType.FILE_URI,
+        quality: 50,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+        encodingType: Camera.EncodingType.JPEG
+      };
+      navigator.camera.getPicture(cameraSuccess, captureError, options);
+    };
+
+    function onFail(error) {
+      $log.error("resolveLocalFileSystemURL: " + error.code);
+      navigator.notification.alert('HomeCtrl::uploadMedia::onFaile:' + 
+                                   error.code, null);
     }
 
-    $scope.$on('$destroy', function() {
-      $scope.passwordModal.remove();
-      $scope.userAgreementModal.remove();
-    });
+    $scope.uploadMedia = function() {
+      $ionicLoading.show({
+        template: "<i class=\"icon ion-loading-a\"></i> Loading..."
+      });
+      console.assert($scope.media);
+
+      // Get hotbar by name
+      /* if ($scope.media.caption && $scope.media.caption.indexOf("@") >= 0) {
+        var caption = $scope.media.caption;
+        var idx = caption.indexOf("@");
+        $scope.media.caption = caption.substr(0, idx).trim();
+        var hotbarName = caption.substr(idx+1);
+        if (hotbarName) {
+          HotBars.getByName(hotbarName, function(err, entity) {
+            if (err) {
+              $log.error("HomeCtrl::uploadMedia::HotBars.getByName:");
+              $log.error(err);
+            } else {
+              $scope.media.hotbar = entity._data;
+            }
+          });
+        }
+      } */
+      // navigator.notification.alert($scope.media.size, null);
+      navigator.notification.alert($scope.media.data, null);
+      if (!$scope.media.hotbar || !$scope.media.hotbar.uuid) {
+        navigator.notification.alert("Invalid HotBar", null);
+        return;
+      }
+      S3.put($scope.media, function(err, data) {
+        if (err) {
+          $ionicLoading.hide();
+          $log.error("HomeCtrl::uploadMedia: " + err);
+          navigator.notification
+            .alert('HomeCtrl::uploadMedia::Error', null);
+          $scope.cleanup();
+        }
+        else {
+          // $log.debug(data);
+          // set media url
+          $scope.media.url =
+            "https://d2x86vdxy89a0s.cloudfront.net/" +
+            $scope.media.filename;
+          // $scope.media.type = "media";
+          // $scope.media.data = null;
+          Activities.create($scope.media, function(err, entity) {
+            if (err) {
+              $log.error("HomeCtrl::create: " + err);
+              $ionicLoading.hide();
+              navigator.notification
+                .alert('HomeCtrl::create::Error', null);
+            } else {
+              $timeout(function() {
+                $scope.media = null;
+                $ionicLoading.hide();
+              });
+            }
+            // cleanup for image
+            if ($scope.media.type === 'image/jpeg')
+              $scope.cleanup();
+          });
+        }
+      });
+    };
+
+    $scope.cleanup = function() {
+      $scope.media = null;
+      if (navigator.camera) {
+        navigator.camera.cleanup(function() {
+          $log.debug("Camera cleanup success");
+          navigator.notification.alert("Camera cleanup success", null);
+        }, function(message) {
+          $log.debug("Failed because: " + message);
+          navigator.notification.alert("Camera cleanup fail", null);
+        });
+      }
+    };
+    $scope.filesChanged = function(elm) {
+      $scope.files = elm.files;
+      $scope.$apply();
+    };
+    $scope.uploadImage = function() {
+      $log.info("uploading image...");
+      $log.debug($scope.files);
+      $log.debug($scope.files[0]);
+      var _user = Global.get("user");
+      // Resize the image to get thumbnail
+      /* var img = new Image();
+         img.src = $scope.files[0];
+         img.onload = function() {
+         var canvas = document.createElement("canvas");
+         new thumbnailer(canvas, img, 188, 3);
+         var data = canvas.toDataURL();
+         } */
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        $timeout(function() {
+          $scope.media = {
+            filename: _user.get('username') + "_" + /* $scope.files[0].name, */
+            Math.round(new Date().getTime()/1000) + ".jpg",
+            data: reader.result,
+            type: "image/jpeg"
+          };
+        });
+      };
+      reader.readAsDataURL($scope.files[0]);
+    };
+
+     //////////////////////////////end of copied home.js copied code
 
     (function() {
       $ionicLoading.show({
